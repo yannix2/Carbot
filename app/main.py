e76f6bbac142b5508f0aa22bb2291883c4b474f4
Yassine Khiari <tasssannas@gmail.com>
2025-04-22 21:05:53 +0100

    final wras omi final

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from api import router
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
-import os
+
 app = FastAPI()
 
 app.add_middleware(
@@ -24,7 +23,3 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Carbot Application!"}
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Render sets PORT dynamically
-    uvicorn.run("main:app", port=port)
